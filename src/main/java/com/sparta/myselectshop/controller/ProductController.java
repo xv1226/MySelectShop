a7e14d42eff658b04e1a80599f7239569d1a7013
K <xv40101392@gmail.com>
2024-06-18 18:51:44 +0900

    관심상품 API 구현

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -7,6 +7,8 @@ import com.sparta.myselectshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -28,5 +30,12 @@ public class ProductController {
     public ProductResponseDto updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
 // 응답 보내기
         return productService.updateProduct(id, requestDto);
+    }
+
+    // 관심 상품 조회하기
+    @GetMapping("/products")
+    public List<ProductResponseDto> getProducts() {
+// 응답 보내기
+        return productService.getProducts();
     }
 }
