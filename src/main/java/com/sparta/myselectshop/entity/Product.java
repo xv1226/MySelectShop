990ff46e568da2699321120afd11ed406b172218
K <xv40101392@gmail.com>
2024-06-18 21:48:56 +0900

    회원별 상품 API 구현

--- src/main/java/com/sparta/myselectshop/entity/Product.java
+++ src/main/java/com/sparta/myselectshop/entity/Product.java
@@ -34,15 +34,20 @@ public class Product extends Timestamped {
     @Column(nullable = false)
     private int myprice;
 
-    public Product(ProductRequestDto requestDto) {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Product(ProductRequestDto requestDto, User user) {
         this.title = requestDto.getTitle();
         this.image = requestDto.getImage();
         this.link = requestDto.getLink();
         this.lprice = requestDto.getLprice();
+        this.user = user;
     }
 
     public void update(ProductMypriceRequestDto requestDto) {
-        this.myprice=requestDto.getMyprice();
+        this.myprice = requestDto.getMyprice();
     }
 
     public void updateByItemDto(ItemDto itemDto) {
