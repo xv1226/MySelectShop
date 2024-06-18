fca05b53612f6d96b3334b65c1a89d3ee6157fb4
K <xv40101392@gmail.com>
2024-06-19 02:03:32 +0900

    폴더 생성 및 조회 구현

--- src/main/java/com/sparta/myselectshop/dto/ProductResponseDto.java
+++ src/main/java/com/sparta/myselectshop/dto/ProductResponseDto.java
@@ -1,18 +1,25 @@
 package com.sparta.myselectshop.dto;
 
 import com.sparta.myselectshop.entity.Product;
+import com.sparta.myselectshop.entity.ProductFolder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
 public class ProductResponseDto {
+
     private Long id;
     private String title;
     private String link;
     private String image;
     private int lprice;
     private int myprice;
+
+    private List<FolderResponseDto> productFolderList = new ArrayList<>();
 
     public ProductResponseDto(Product product) {
         this.id = product.getId();
@@ -21,5 +28,8 @@ public class ProductResponseDto {
         this.image = product.getImage();
         this.lprice = product.getLprice();
         this.myprice = product.getMyprice();
+        for (ProductFolder productFolder : product.getProductFolderList()) {
+            productFolderList.add(new FolderResponseDto(productFolder.getFolder()));
+        }
     }
 }
