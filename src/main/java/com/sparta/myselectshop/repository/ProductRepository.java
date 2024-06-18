c8e9f2f708843d0215abdef6cf6682a8301d14f5
K <xv40101392@gmail.com>
2024-06-19 02:41:56 +0900

    폴더 별 관심상품 조회 구현

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -13,4 +13,5 @@ public interface ProductRepository extends JpaRepository<Product,Long> {
 
     Page<Product> findAllByUser(User user, Pageable pageable);
 
+    Page<Product> findAllByUserAndProductFolderList_FolderId(User user, Long folderId, Pageable pageable);
 }
